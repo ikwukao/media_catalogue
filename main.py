22a985d72652c4167f90ad453da3ea2d01ef9950
Ikwuka Okoye <ikwukao@proton.me>
2026-05-30 17:56:42 +0100

    Create Movie class

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-class MediaCatalogue:
-    pass
+class Movie:
+    def __init__(self, title, year, director, duration):
+        self.title = title
+        self.year = year
+        self.director = director
+        self.duration = duration

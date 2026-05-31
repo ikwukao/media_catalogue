878e66c7b22a15252c6e36f199fbbe193bbab437
Ikwuka Okoye <ikwukao@proton.me>
2026-05-31 23:30:24 +0100

    Format main.py with ruff

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 class Movie:
-    def __init__(self, title, year, director, duration):    
+    def __init__(self, title, year, director, duration):
         self.title = title
         self.year = year
         self.director = director
         self.duration = duration
-    
-    def __str__(self):
-        return f'{self.title} ({self.year}) - {self.duration} min, {self.director}'
 
-movie1 = Movie('The Matrix', 1999, 'The Wachowskis', 136)
+    def __str__(self):
+        return f"{self.title} ({self.year}) - {self.duration} min, {self.director}"
+
+
+movie1 = Movie("The Matrix", 1999, "The Wachowskis", 136)
 print(movie1)
